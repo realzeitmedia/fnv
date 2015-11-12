56470bdfd0b9c8f6f1d8863236aa710baff2355a
Harmen <harm@dds.nl>
2015-11-12 11:39:41 +0100

    more test cases

--- fnv_test.go
+++ fnv_test.go
@@ -20,7 +20,50 @@ func TestString(t *testing.T) {
 		ours = ourlib.Add(ours, src)
 
 		if have, want := ours, theirs; have != want {
-			t.Errorf("have %d, want %d. Case %q", src)
+			t.Errorf("have %d, want %d. case %q", have, want, src)
+		}
+	}
+}
+
+func TestStrings(t *testing.T) {
+	for _, src := range [][]string{
+		{},
+		{""},
+		{"hello", "world"},
+	} {
+		h := theirlib.New64a()
+		for _, s := range src {
+			h.Write([]byte(s))
+		}
+		theirs := h.Sum64()
+
+		ours := ourlib.New()
+		for _, s := range src {
+			ours = ourlib.Add(ours, s)
+		}
+
+		if have, want := ours, theirs; have != want {
+			t.Errorf("have %d, want %d. case %q", have, want, src)
+		}
+	}
+}
+
+func TestByte(t *testing.T) {
+	for _, src := range []string{
+		"",
+		"hello world",
+	} {
+		h := theirlib.New64a()
+		h.Write([]byte(src))
+		theirs := h.Sum64()
+
+		ours := ourlib.New()
+		for i := 0; i < len(src); i++ {
+			ours = ourlib.AddByte(ours, src[i])
+		}
+
+		if have, want := ours, theirs; have != want {
+			t.Errorf("have %d, want %d. case %q", have, want, src)
 		}
 	}
 }
